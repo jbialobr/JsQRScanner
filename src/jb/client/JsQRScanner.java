73e9388348acdb79caf102c94a35d99e0b184651
Janusz Białobrzewski <jbialobr@o2.pl>
2016-07-29 21:28:38 +0200

    Init

---
+++ src/jb/client/JsQRScanner.java
@@ -0,0 +1,17 @@
+package jb.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class JsQRScanner implements EntryPoint
+{
+    /**
+     * This is the entry point method.
+     */
+    public void onModuleLoad()
+    {
+
+    }
+}
